532d58ed8367e24f10af800d05fe2a183f9d3db9
xudong.yang <xudong.yang@husor.com>
2018-10-09 13:55:15 +0800

    add new files

--- preprocessing/BinaryLinearClassifier.py
+++ preprocessing/BinaryLinearClassifier.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import os
+import operator
 from model.Params import Params
 
 _CSV_COLUMNS = "content,id,location_traffic_convenience,location_distance_from_business_district,location_easy_to_find,\
@@ -13,8 +14,8 @@ _CSV_DEFAULTS = [[""], [0], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2]
                  [-2], [-2], [-2], [-2], [-2], [-2], [-2], [""]]
 
 flags = tf.app.flags
-flags.DEFINE_string("data_dir", "..\\data", "Directory containing the dataset.")
-flags.DEFINE_string("model_dir", "..\\experiments\\TextCNN", "Base directory for the model.")
+flags.DEFINE_string("data_dir", "../data", "Directory containing the dataset.")
+flags.DEFINE_string("model_dir", "../experiments/linear", "Base directory for the model.")
 flags.DEFINE_string("gpu", "0", "which gpu to use.")
 flags.DEFINE_integer("save_checkpoints_steps", 1000, "Save checkpoints every this many steps")
 flags.DEFINE_integer("throttle_secs", 300, "evaluation time span in seconds")
@@ -29,7 +30,7 @@ def parse_line(line, vocab, max_len, target):
   content_length = tf.size(content_words)
   content_words = tf.slice(content_words, [0], [tf.minimum(content_length, max_len)])
   content_ids = vocab.lookup(content_words)
-  return content_ids, features[target]
+  return {"x":content_ids}, features[target]
 
 
 def transform_label(x, y):
@@ -45,24 +46,52 @@ def input_fn(path_csv, path_vocab, target, params, shuffle_buffer_size):
   Returns:
       dataset: (tf.Dataset) yielding list of ids of tokens and labels for each example
   """
-  #vocab = tf.contrib.lookup.index_table_from_file(path_vocab, num_oov_buckets=params.num_oov_buckets)
-  #params.id_pad_word = vocab.lookup(tf.constant(params.pad_word))
+  vocab = tf.contrib.lookup.index_table_from_file(path_vocab, num_oov_buckets=params.num_oov_buckets)
+  params.id_pad_word = vocab.lookup(tf.constant(params.pad_word))
   # Load txt file, one example per line
   dataset = tf.data.TextLineDataset(path_csv)
   # Convert line into list of tokens, splitting by white space
-  dataset = dataset.skip(1).map(lambda line: parse_line(line, path_vocab, params.sentence_max_len, target)) # skip the header
+  dataset = dataset.skip(1).map(lambda line: parse_line(line, vocab, params.sentence_max_len, target)) # skip the header
   dataset = dataset.filter(lambda x, y: tf.logical_or(tf.equal(y, -1), tf.equal(y, 1)))
   dataset = dataset.map(lambda x, y: transform_label(x, y))
-  #if shuffle_buffer_size > 0:
-  #  dataset = dataset.shuffle(shuffle_buffer_size).repeat()
+  if shuffle_buffer_size > 0:
+    dataset = dataset.shuffle(shuffle_buffer_size).repeat()
   # Create batches and pad the sentences of different length
-  padded_shapes = ([params.sentence_max_len], [])
-  #padding_values = (params.id_pad_word, 0)
-  padding_values = (tf.cast(0, tf.int64), 0)
+  padded_shapes = ({"x": tf.TensorShape([params.sentence_max_len])}, [])
+  #padding_values = ({"x": params.id_pad_word}, 0)
+  padding_values = ({"x": tf.cast(params.vocab_size, tf.int64)}, 0)
   dataset = dataset.padded_batch(params.batch_size, padded_shapes, padding_values).prefetch(1)
   print(dataset.output_types)
   print(dataset.output_shapes)
   return dataset
+
+def train_with_target(target, params):
+  path_words = os.path.join(FLAGS.data_dir, 'words.txt')
+  path_train = os.path.join(FLAGS.data_dir, 'train.csv')
+  path_eval = os.path.join(FLAGS.data_dir, 'valid.csv')
+  #test(path_eval, path_words, params)
+  column = tf.feature_column.categorical_column_with_identity('x', params.vocab_size)
+  model_dir = os.path.join((FLAGS.model_dir, target))
+  config = tf.estimator.RunConfig(model_dir=model_dir, save_checkpoints_steps=FLAGS.save_checkpoints_steps)
+  classifier = tf.estimator.LinearClassifier(feature_columns=[column], config=config)
+  train_spec = tf.estimator.TrainSpec(
+    input_fn=lambda: input_fn(path_train, path_words, target, params, params.shuffle_buffer_size),
+    max_steps=params.train_steps
+  )
+  eval_spec = tf.estimator.EvalSpec(
+    input_fn=lambda: input_fn(path_eval, path_words, target, params, 0),
+    throttle_secs=FLAGS.throttle_secs
+  )
+  print("before train and evaluate")
+  tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
+  print("after train and evaluate")
+  weights = classifier.get_variable_value('linear/linear_model/x/weights').flatten()
+  id2word = tf.contrib.lookup.index_to_string_table_from_file(path_words, params.vocab_size)
+  size = len(weights)
+  weightTable = {id2word.lookup(i): weights[i] for i in range(size)}
+  words = sorted(weightTable.items(), key=operator.itemgetter(1), reverse=True)
+  output_file = os.path.join(model_dir, "word_weights.txt")
+  write_dict(words, output_file)
 
 
 def main(unused_argv):
@@ -74,12 +103,21 @@ def main(unused_argv):
   assert os.path.isfile(json_path), "No json file found at {}, run build_vocab.py".format(json_path)
   params.update(json_path)
 
-  path_words = os.path.join(FLAGS.data_dir, 'words.txt')
-  path_train = os.path.join(FLAGS.data_dir, 'train.csv')
-  path_eval = os.path.join(FLAGS.data_dir, 'valid.csv')
+  ignore = set(["content", "id", "content_ws"])
+  targets = [x for x in _CSV_COLUMNS if x not in ignore]
+  for target in targets:
+    train_with_target(target, params)
 
-  vocab = tf.contrib.lookup.index_table_from_file(path_words, num_oov_buckets=params.num_oov_buckets)
-  dataset = input_fn(path_eval, vocab, "service_wait_time", params, params.shuffle_buffer_size)
+
+def write_dict(dict, output_file):
+  print("writing to " + output_file)
+  output = open(output_file, 'w')
+  for word, score in dict:
+    output.write("{}\t{}\n".format(word, score))
+  output.close()
+
+def test(path_eval, path_words, params):
+  dataset = input_fn(path_eval, path_words, "service_wait_time", params, params.shuffle_buffer_size)
   iterator = dataset.make_initializable_iterator()
   iter_init_op = iterator.initializer
   one_element = iterator.get_next()
@@ -87,7 +125,6 @@ def main(unused_argv):
   with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     sess.run(tf.tables_initializer())
-    #params.id_pad_word = vocab.lookup(tf.constant(params.pad_word))
     # print(sess.run(mask))
     sess.run(iter_init_op)
     for i in range(4):
@@ -96,6 +133,5 @@ def main(unused_argv):
 if __name__ == '__main__':
   if "CUDA_VISIBLE_DEVICES" in os.environ:
     print("CUDA_VISIBLE_DEVICES:", os.environ["CUDA_VISIBLE_DEVICES"])
-  #tf.logging.set_verbosity(tf.logging.INFO)
-  #tf.app.run(main=main)
-  main(1)
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.app.run(main=main)
