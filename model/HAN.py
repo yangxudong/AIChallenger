0bfbd0c447bd93344e00168e0c03359384c8aa44
xudong.yang <xudong.yang@husor.com>
2018-10-15 17:48:06 +0800

    add new files

---
+++ model/HAN.py
@@ -0,0 +1,117 @@
+# coding=utf8
+import tensorflow as tf
+from tensorflow.python.estimator.canned import optimizers
+
+def length(sequences):
+  # 返回一个序列中每个元素的长度
+  used = tf.sign(tf.reduce_max(tf.abs(sequences), reduction_indices=2))
+  seq_len = tf.reduce_sum(used, reduction_indices=1)
+  return tf.cast(seq_len, tf.int32)
+
+
+class HAN(tf.estimator.Estimator):
+  def __init__(self,
+    params,
+    model_dir=None,
+    optimizer='Adagrad',
+    config=None,
+    warm_start_from=None,
+  ):
+    ''' an implement of Hierarchical Attention Networks for Document Classification '''
+    self.params = params
+    if not optimizer: optimizer = 'Adagrad'
+    self.optimizer = optimizers.get_optimizer_instance(optimizer, params.learning_rate)
+
+    def _model_fn(features, labels, mode, config):
+      # 构建模型
+      word_embedded = self.word2vec(params, features["content"])
+      sent_vec = self.sent2vec(word_embedded, features["sentence_len"])
+      doc_vec = self.doc2vec(sent_vec, features["sentence_num"])
+
+      logits = self.classifer(doc_vec)
+      my_head = tf.contrib.estimator.multi_class_head(params.num_classes)
+      return my_head.create_estimator_spec(
+        features=features,
+        mode=mode,
+        labels=labels,
+        logits=logits,
+        train_op_fn=lambda loss: self.optimizer.minimize(loss, global_step=tf.train.get_global_step())
+      )
+
+    super(HAN, self).__init__(
+      model_fn=_model_fn, model_dir=model_dir, config=config,
+      warm_start_from=warm_start_from)
+
+  def word2vec(self, x):
+    # 嵌入层
+    with tf.name_scope("embedding"):
+      # Get word embeddings for each token in the sentence
+      embedding_mat = tf.get_variable(name="embeddings", dtype=tf.float32,
+                                   shape=[self.params.vocab_size, self.params.embedding_size])
+      # shape为[batch_size, sent_in_doc, word_in_sent, embedding_size]
+      word_embedded = tf.nn.embedding_lookup(embedding_mat, x)
+    return word_embedded
+
+  def sent2vec(self, word_embedded, word_num):
+    with tf.name_scope("sent2vec"):
+      # GRU的输入tensor是[batch_size, max_time, ...].在构造句子向量时max_time应该是每个句子的长度，所以这里将
+      # batch_size * sent_in_doc当做是batch_size.这样一来，每个GRU的cell处理的都是一个单词的词向量
+      # 并最终将一句话中的所有单词的词向量融合（Attention）在一起形成句子向量
+
+      # shape为[batch_size*sent_in_doc, word_in_sent, embedding_size]
+      word_embedded = tf.reshape(word_embedded, [-1, self.params.max_sentence_length, self.params.embedding_size])
+      word_num = tf.reshape(word_num, [-1])
+      # shape为[batch_size*sent_in_doce, word_in_sent, hidden_size*2]
+      word_encoded = self.BidirectionalGRUEncoder(word_embedded, word_num, name='word_encoder')
+      # shape为[batch_size*sent_in_doc, hidden_size*2]
+      sent_vec = self.AttentionLayer(word_encoded, name='word_attention')
+      return sent_vec
+
+  def doc2vec(self, sent_vec, sent_num):
+    # 原理与sent2vec一样，根据文档中所有句子的向量构成一个文档向量
+    with tf.name_scope("doc2vec"):
+      sent_vec = tf.reshape(sent_vec, [-1, self.params.max_sentence_num, self.params.hidden_size * 2])
+      # shape为[batch_size, sent_in_doc, hidden_size*2]
+      doc_encoded = self.BidirectionalGRUEncoder(sent_vec, sent_num, name='sent_encoder')
+      # shape为[batch_szie, hidden_szie*2]
+      doc_vec = self.AttentionLayer(doc_encoded, name='sent_attention')
+      return doc_vec
+
+  def classifer(self, doc_vec):
+    # 最终的输出层，是一个全连接层
+    with tf.name_scope('doc_classification'):
+      out = tf.layers.dense(inputs=doc_vec, num_outputs=self.params.num_classes, activation_fn=None)
+      return out
+
+  def BidirectionalGRUEncoder(self, inputs, sequence_len, name):
+    # 双向GRU的编码层，将一句话中的所有单词或者一个文档中的所有句子向量进行编码得到一个 2×hidden_size的输出向量，
+    # 然后在经过Attention层，将所有的单词或句子的输出向量加权得到一个最终的句子/文档向量。
+    # 输入inputs的shape是[batch_size, max_time, voc_size]
+    with tf.variable_scope(name):
+      GRU_cell_fw = tf.nn.rnn_cell.GRUCell(self.params.hidden_size)
+      GRU_cell_bw = tf.nn.rnn_cell.GRUCell(self.params.hidden_size)
+      # fw_outputs和bw_outputs的size都是[batch_size, max_time, hidden_size]
+      ((fw_outputs, bw_outputs), (_, _)) = tf.nn.bidirectional_dynamic_rnn(cell_fw=GRU_cell_fw,
+                                                                           cell_bw=GRU_cell_bw,
+                                                                           inputs=inputs,
+                                                                           sequence_length=sequence_len,
+                                                                           dtype=tf.float32)
+      # outputs的size是[batch_size, max_time, hidden_size*2]
+      outputs = tf.concat((fw_outputs, bw_outputs), 2)
+      return outputs
+
+  def AttentionLayer(self, inputs, name):
+    # inputs是GRU的输出，size是[batch_size, max_time, encoder_size(hidden_size * 2)]
+    with tf.variable_scope(name):
+      # u_context是上下文的重要性向量，用于区分不同单词/句子对于句子/文档的重要程度,
+      # 因为使用双向GRU，所以其长度为2×hidden_szie
+      hidden_size = self.params.hidden_size * 2
+      u_context = tf.get_variable('u_context', [hidden_size], dtype=tf.float32)
+      # 使用一个全连接层编码GRU的输出的到期隐层表示,输出h的size是[batch_size, max_time, hidden_size * 2]
+      h = tf.layers.dense(inputs, units=hidden_size, activation=tf.nn.tanh)
+      # shape为[batch_size, max_time, 1]
+      #alpha = tf.nn.softmax(tf.reduce_sum(tf.multiply(h, u_context), axis=2, keep_dims=True), dim=1)
+      alpha = tf.nn.softmax(tf.tensordot(h, u_context, [[-1], [0]])) # shape = [batch_size, max_time]
+      # reduce_sum之前shape为[batch_szie, max_time, hidden_szie*2]，之后shape为[batch_size, hidden_size*2]
+      atten_output = tf.reduce_sum(tf.multiply(inputs, alpha), axis=1)
+      return atten_output
---
+++ model/doc_input_fn.py
@@ -0,0 +1,54 @@
+"""Create the input data pipeline using `tf.data`"""
+import tensorflow as tf
+
+_CSV_COLUMNS = "id,content,location_traffic_convenience,location_distance_from_business_district,location_easy_to_find,\
+service_wait_time,service_waiters_attitude,service_parking_convenience,service_serving_speed,\
+price_level,price_cost_effective,price_discount,\
+environment_decoration,environment_noise,environment_space,environment_cleaness,\
+dish_portion,dish_taste,dish_look,dish_recommendation,\
+others_overall_experience,others_willing_to_consume_again,formatted_content,sentences,sentence_len".split(",")
+
+_CSV_DEFAULTS = [[""], [0], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2], [-2],
+                 [-2], [-2], [-2], [-2], [-2], [-2], [-2], [""]]
+
+def parse_line(line, vocab, pad_word, label):
+  columns = tf.decode_csv(line, _CSV_DEFAULTS, field_delim=',')
+  features = dict(zip(_CSV_COLUMNS, columns))
+  sentences = tf.string_split([features["sentences"]], sep="\n").values
+  words = tf.string_split(sentences)
+  word_mat = tf.sparse_tensor_to_dense(words, default_value=pad_word)
+  word_ids = vocab.lookup(word_mat)
+  sentence_len = tf.string_to_number(tf.string_split([features["sentence_len"]], sep=",").values, out_type=tf.int32)
+  sentence_num = tf.size(sentence_len)
+  out_features = {"content": word_ids, "sentence_len": sentence_len, "sentence_num": sentence_num}
+  return out_features, features[label] + 2
+
+
+def input_fn(path_csv, path_vocab, label, params, shuffle_buffer_size):
+  """Create tf.data Instance from csv file
+  Args:
+      path_csv: (string) path containing one example per line
+      vocab: (tf.lookuptable)
+  Returns:
+      dataset: (tf.Dataset) yielding list of ids of tokens and labels for each example
+  """
+  vocab = tf.contrib.lookup.index_table_from_file(path_vocab, num_oov_buckets=params.num_oov_buckets)
+  params.id_pad_word = vocab.lookup(tf.constant(params.pad_word))
+  # Load txt file, one example per line
+  dataset = tf.data.TextLineDataset(path_csv)
+  # Convert line into list of tokens, splitting by white space
+  dataset = dataset.skip(1).map(lambda line: parse_line(line, vocab, params.pad_word, label)) # skip the header
+  if shuffle_buffer_size > 0:
+    dataset = dataset.shuffle(shuffle_buffer_size).repeat()
+  # Create batches and pad the sentences of different length
+  feature_shapes = {"content": tf.TensorShape([params.max_doc_len, params.max_sentence_len]),
+                    "sentence_len": tf.TensorShape([params.max_doc_len]),
+                    "sentence_num": tf.TensorShape([])}
+  pad_values = {"content": params.id_pad_word, "sentence_len": 0, "sentence_num": 0}
+  padded_shapes = (feature_shapes, [])
+  padding_values = (pad_values, 0)
+  dataset = dataset.padded_batch(params.batch_size, padded_shapes, padding_values).prefetch(1)
+  print(dataset.output_types)
+  print(dataset.output_shapes)
+  return dataset
+
